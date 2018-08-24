088ef7946525e3a0020219cf5bff0272d71ec094
Pavel Shipitsyn <pavship@yandex.ru>
2018-08-24 11:27:39 +0300

    with user auth

--- src/resolvers/Mutation/auth.js
+++ src/resolvers/Mutation/auth.js
@@ -24,19 +24,24 @@ const auth = {
 	},
 
 	async login(parent, { email, password }, ctx, info) {
-		const user = await ctx.db.query.user({ where: { email } })
+        const user = await ctx.db.query.user({ where: { email } }, '{id email password person { fName lName }}')
 		if (!user) {
-		throw new Error(`No such user found for email: ${email}`)
+            throw new Error(`No such user found for email: ${email}`)
 		}
-
 		const valid = await bcrypt.compare(password, user.password)
 		if (!valid) {
-		throw new Error('Invalid password')
+            throw new Error('Invalid password')
 		}
-
+        console.log(user)
 		return {
-		token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
-		user,
+            token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+            user
+            // user: {
+            //     email: user.email,
+            //     person: {
+            //         fName: user.person.fName
+            //     }
+            // }
 		}
 	},
 }
