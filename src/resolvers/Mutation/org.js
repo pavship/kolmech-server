2f6988d6144cdc85f176298b99e90c02a7d23daa
Pavel Shipitsyn <pavship@yandex.ru>
2018-09-02 15:46:23 +0300

    deleteAllOrgs calls MoeDelo API sequentially
    otherwise not all orgs may be deleted

--- src/resolvers/Mutation/org.js
+++ src/resolvers/Mutation/org.js
@@ -5,7 +5,10 @@ const { toLocalTimestamp } = require('../../utils/dates')
 
 const org = {
 	async createOrg(_, { inn }, ctx, info) {
-		const userId = getUserId(ctx)
+        const userId = getUserId(ctx)
+        // check org with provided INN doesn't exist
+        const orgExists = await ctx.db.exists.Org({ inn })
+        if (orgExists) throw new Error(`Организация с инн ${inn} уже существует в базе`)
         // Create contractor in the Moedelo
         const url = baseURL + '/inn'
         // @ts-ignore
@@ -21,7 +24,6 @@ const org = {
             }
         })
         const json = await response.json()
-        console.log(json)
         if (!json.Id) throw new Error('МоеДело API error: ' + json.Message)
         const { Id: moedeloId, Name: name, LegalAddress: legalAddress } = json
         // return {
@@ -40,12 +42,10 @@ const org = {
 		}, info)
     },
 
-    // delete all orgs from prisma db and corresponding orgs from MoeDelo
-    async deleteAllOrgs(_, __, ctx, info) {
-        const orgs = await ctx.db.query.orgs({}, '{ id moedeloId }')
-        await Promise.all(orgs.map(async (o) => {
+    async deleteOrg(_, { id }, ctx, info) {
+        const org = await ctx.db.query.org({where: {id}}, '{ id moedeloId }')
+        await Promise.all([org].map(async o => {
             const url = baseURL + '/' + o.moedeloId
-            // console.log(url)
             // @ts-ignore
             const response = await fetch(url, {
                 method: 'DELETE',
@@ -54,8 +54,29 @@ const org = {
                     'Content-Type': 'application/json'
                 }
             })
-            // console.log('response > ', Object.keys(response)) //response object contains keys: [ 'size', 'timeout' ]
+            // console.log('o.moedeloId > ', o.moedeloId)
+            // console.log('response > ', response)
         }))
+        return ctx.db.mutation.deleteOrg({ where: { id } }, info)
+    },
+
+    // delete all orgs from prisma db and corresponding orgs from MoeDelo
+    async deleteAllOrgs(_, __, ctx, info) {
+        const orgs = await ctx.db.query.orgs({}, '{ id moedeloId }')
+        // calls to MoeDelo API are made sequentially (otherwise not all orgs may be deleted)
+        for (let o of orgs) {
+            const url = baseURL + '/' + o.moedeloId
+            // @ts-ignore
+            const response = await fetch(url, {
+                method: 'DELETE',
+                headers: { 
+                    'md-api-key': process.env.MOEDELO_SECRET,
+                    'Content-Type': 'application/json'
+                }
+            })
+            // console.log('o.moedeloId > ', o.moedeloId)
+            // console.log('response > ', response)
+        }
         return ctx.db.mutation.deleteManyOrgs({ where: { id_in: orgs.map(o => o.id) } }, info)
     },
 }
