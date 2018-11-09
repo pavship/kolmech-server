1457aaf69beaf6a9be770e055984e25a345f8735
Pavel Shipitsyn <pavship@yandex.ru>
2018-11-09 19:01:47 +0300

    running on prisma@1.19.3
    1.20.1 fails to dploy with error ENOENT

--- src/resolvers/Mutation/mixed.js
+++ src/resolvers/Mutation/mixed.js
@@ -1,17 +1,17 @@
 const { auth } = require('./auth')
 
 const mixed = {
-	async signupAndCreateEnquiry(_, args, ctx, info) {
-		try {
-			const {email, regName: fName} = args
-			const password = 'yoyopass'
-			const signup = auth.signup(_, {email, password, fName, lName}, ctx, '{ token }')
-			console.log('signup > ', signup)
-		} catch (err) {
-			console.log(err)
-			return null
-		}
-	},
+	// async signupAndCreateEnquiry(_, args, ctx, info) {
+	// 	try {
+	// 		const {email, regName: fName} = args
+	// 		const password = 'yoyopass'
+	// 		const signup = auth.signup(_, {email, password, fName, lName}, ctx, '{ token }')
+	// 		console.log('signup > ', signup)
+	// 	} catch (err) {
+	// 		console.log(err)
+	// 		return null
+	// 	}
+	// },
 
 }
 
