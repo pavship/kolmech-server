89db636c33eb60c899b45b616d4eca8d09edc5e8
Pavel Shipitsyn <pavship@yandex.ru>
2019-04-29 23:02:33 +0300

    + improved syncWithTochkaPayments
    keep consistent counterparties (orgs) between App db, Tochka and MoeDelo. Better code modularity

--- src/resolvers/Mutation/org.js
+++ src/resolvers/Mutation/org.js
@@ -1,30 +1,97 @@
 const fetch = require('node-fetch')
 const moedeloBaseUrl = 'https://restapi.moedelo.org/kontragents/api/v1/kontragent'
-const { toLocalTimestamp } = require('../../utils/dates')
+
+const headers = {
+	'md-api-key': process.env.MOEDELO_SECRET,
+	'Content-Type': 'application/json'
+}
+
+const createMoeDeloOrg = async inn => {
+	const createOrgUrl = moedeloBaseUrl + '/inn'
+	// @ts-ignore
+	createOrgResponse = await fetch(createOrgUrl, {
+		method: 'POST',
+		body: JSON.stringify({
+			"Inn": inn,
+			"Type": 1
+		}),
+		headers
+	})
+	const statusText = createOrgResponse.statusText
+	if (statusText !== 'Created') throw new Error('Ошибка сервера МоеДело. Статус запроса createMoeDeloOrg: ' + statusText)
+	const { Id: moedeloId, Name: name, LegalAddress: legalAddress } = await createOrgResponse.json()
+	return {
+		inn,
+		legalAddress,
+		moedeloId,
+		name,
+	}
+}
 
 const org = {
+	async checkOrgs(_, __, ctx, info) {
+		const orgs = await ctx.db.query.orgs({}, '{ id inn moedeloId }')
+		const moeDeloOrgs = await org.getMoeDeloOrgs(_, __, ctx, info)
+		const sortedOrgs = moeDeloOrgs.sort((a, b) => a.inn < b.inn)
+		const orgsByInn = sortedOrgs.reduce((res, o, i) => {
+			if (i === 0) return [[o]]
+			const curInn = res[res.length-1][0].inn
+			o.inn === curInn
+				? res[res.length - 1].push(o)
+				: res[res.length] = [o]
+			return res
+		}, [])
+		toDelete = orgsByInn
+			.map(orgs => orgs.sort((a, b) => a.moedeloId < b.moedeloId))
+			.map(orgs => {
+				return orgs.length === 1
+					? []
+					: orgs.slice(1)
+					// .map(o => o.moedeloId)
+			})
+			.reduce((res, arr) => [...res, ...arr])
+		console.log('toDelete > ', toDelete)
+		
+		console.log('sortedOrgs > ', sortedOrgs)
+		// console.log('excessOrgs > ', excessOrgs)
+	},
 	async createOrg(_, { inn }, ctx, info) {
 		const { userId, db } = ctx
 		// if org with provided INN is in db already, then just return it
 		const foundOrg = await db.query.org({ where: { inn } }, info)
-		if (foundOrg) return foundOrg
-		// Create contractor in the Moedelo
-		const url = moedeloBaseUrl + '/inn'
+		// if (foundOrg) return foundOrg
+		// Check if contractor exists in the Moedelo
 		// @ts-ignore
-		const response = await fetch(url, {
-			method: 'POST',
-			body: JSON.stringify({
-				"Inn": inn,
-				"Type": 2
-			}),
-			headers: { 
-				'md-api-key': process.env.MOEDELO_SECRET,
-				'Content-Type': 'application/json'
+		// const orgs = await org.getMoeDeloOrgs(_, {}, ctx, '{ id inn name }')
+		// const existingOrg = orgs.find(o => o.Inn === inn)
+		const existingOrg = null
+		let createdOrg = null
+		if (!existingOrg) {
+			// Create contractor in the Moedelo
+			const createOrgUrl = moedeloBaseUrl + '/inn'
+			// @ts-ignore
+			createOrgResponse = await fetch(createOrgUrl, {
+				method: 'POST',
+				body: JSON.stringify({
+					"Inn": inn,
+					"Type": 1
+				}),
+				headers
+			})
+			const statusText = createOrgResponse.statusText
+			if (statusText !== 'OK') throw new Error('Ошибка сервера МоеДело. Статус запроса: ' + statusText)
+			const createdOrg = await createOrgResponse.json()
+			console.log('got org > ', createdOrg)
+		}
+		const { Id: moedeloId, Name: name, LegalAddress: legalAddress } = existingOrg || createdOrg
+		if (foundOrg) return db.mutation.updateOrg({
+			where: { id: foundOrg.id },
+			data: {
+				moedeloId,
+				name,
+				legalAddress
 			}
-		})
-		const json = await response.json()
-		if (!json.Id) throw new Error('МоеДело API error: ' + json.Message)
-		const { Id: moedeloId, Name: name, LegalAddress: legalAddress } = json
+		}, info)
 		return db.mutation.createOrg({
 			data: {
 				inn,
@@ -34,7 +101,6 @@ const org = {
 			}
 		}, info)
 	},
-
 	async deleteOrg(_, { id }, ctx, info) {
 		const org = await ctx.db.query.org({where: {id}}, '{ id moedeloId }')
 		await Promise.all([org].map(async o => {
@@ -42,17 +108,13 @@ const org = {
 			// @ts-ignore
 			const response = await fetch(url, {
 				method: 'DELETE',
-				headers: { 
-					'md-api-key': process.env.MOEDELO_SECRET,
-					'Content-Type': 'application/json'
-				}
+				headers
 			})
 			// console.log('o.moedeloId > ', o.moedeloId)
 			// console.log('response > ', response)
 		}))
 		return ctx.db.mutation.deleteOrg({ where: { id } }, info)
 	},
-
 	// delete all orgs from prisma db and corresponding orgs from MoeDelo
 	async deleteAllOrgs(_, __, ctx, info) {
 		const orgs = await ctx.db.query.orgs({}, '{ id moedeloId }')
@@ -62,16 +124,69 @@ const org = {
 			// @ts-ignore
 			const response = await fetch(url, {
 				method: 'DELETE',
-				headers: { 
-					'md-api-key': process.env.MOEDELO_SECRET,
-					'Content-Type': 'application/json'
-				}
+				headers
 			})
 			// console.log('o.moedeloId > ', o.moedeloId)
 			// console.log('response > ', response)
 		}
 		return ctx.db.mutation.deleteManyOrgs({ where: { id_in: orgs.map(o => o.id) } }, info)
 	},
+	async getMoeDeloOrgs(_, __, ctx, info) {
+		const allOrgsResponse = await fetch(moedeloBaseUrl, {
+			method: 'GET',
+			headers
+		})
+		const statusText = allOrgsResponse.statusText
+		if (statusText !== 'OK') throw new Error('Ошибка сервера МоеДело. Статус запроса: ' + statusText)
+		const { ResourceList: orgs } = await allOrgsResponse.json()
+		console.log('got orgs > ', orgs.length || orgs)
+		return orgs.map(({
+			Id: moedeloId,
+			Inn: inn,
+			LegalAddress: legalAddress,
+			Name: name,
+		}) => ({
+			inn,
+			legalAddress,
+			moedeloId,
+			name,
+		}))
+	},
+	async upsertOrgsByInn(_, inns, ctx, info) {
+		const { userId, db } = ctx
+		console.log('inns > ', inns)
+		let moeDeloOrgs = await org.getMoeDeloOrgs(_, {}, ctx, '{ moedeloId inn name }')
+		console.log('moeDeloOrgs > ', moeDeloOrgs)
+		const existedOrgs = moeDeloOrgs.filter(o => inns.includes(o.inn))
+		const createdOrgs = []
+		console.log('inns.filter(inn => moeDeloOrgs.findIndex(o => o.inn === inn) === -1) > ', inns.filter(inn => moeDeloOrgs.findIndex(o => o.inn === inn) === -1))
+		for (let inn of inns.filter(inn => moeDeloOrgs.findIndex(o => o.inn === inn) === -1)) {
+			createdOrgs.push(await createMoeDeloOrg(inn))
+		}
+		moeDeloOrgs = [...existedOrgs, ...createdOrgs]
+		console.log('moeDeloOrgs > ', moeDeloOrgs)
+		return await Promise.all(moeDeloOrgs.map(({
+			inn,
+			legalAddress,
+			moedeloId,
+			name,
+		}) => 
+			db.mutation.upsertOrg({
+				where: { inn },
+				create: {
+					inn,
+					legalAddress,
+					moedeloId,
+					name,
+				},
+				update: {
+					legalAddress,
+					moedeloId,
+					name,
+				}
+			})
+		))
+	}
 }
 
 module.exports = { org }
--- src/resolvers/Mutation/tochka.js
+++ src/resolvers/Mutation/tochka.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const { toLocalISOString } = require('../../utils/dates')
+const { org: { createOrg, getMoeDeloOrgs, upsertOrgsByInn } } = require('./org')
 
 const headers = {
   'Authorization': `Bearer ${process.env.TOCHKA_API_TOKEN}`,
@@ -8,118 +9,155 @@ const headers = {
   'Content-Type': 'application/json'
 }
 
-const tochka = {
-	async syncWithTochkaPayments(_, __, ctx, info) {
-    // function settings
-    const mode = 'syncAll' // TODO add 'syncNew' mode
-    const requestUrl = 'https://enter.tochka.com/api/v1/statement'
-    try {
-      const { userId, db } = ctx
-      // 1. fetch request_id from tochka
-      const requestResponse = await fetch(requestUrl, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          account_code: process.env.TOCHKA_ACCOUNT_CODE_IP,
-          bank_code: '044525999',
-          date_start: '2019-01-01',
-          date_end: toLocalISOString(new Date).slice(0,10)
-        })
+const fetchTochkaPayments = async () => {
+  const baseUrl = 'https://enter.tochka.com/api/v1/statement'
+    // 1. get request_id
+    const requestResponse = await fetch(baseUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        account_code: process.env.TOCHKA_ACCOUNT_CODE_IP,
+        bank_code: '044525999',
+        date_start: '2019-01-01',
+        date_end: toLocalISOString(new Date).slice(0,10)
       })
-      const { request_id } = await requestResponse.json()
-      // 2. fetch payments from tochka
-      const statementUrl = `https://enter.tochka.com/api/v1/statement/result/${request_id}`
-      const statementResponse = await fetch(statementUrl, {
-        method: 'GET',
-        headers
-      })
-      // console.log('response > ', statementResponse)
-      // console.log('statementResponse.headers > ', statementResponse.headers)
-      const { payments: tochkaPayments } = await statementResponse.json()
-      // console.log('tochkaPayments > ', JSON.stringify(tochkaPayments,null,2))
-      // 3. count number of payments for each day and assign number (like 1,2,3,etc..) to each payment
-      paymentDateCounter = {}
-      const tochkaPaymentsAugmented = tochkaPayments.map(p => {
-        const prevCount = paymentDateCounter[p.payment_date] || 0
-        const count = prevCount + 1
-        paymentDateCounter[p.payment_date] = count
-        return {
-          ...p,
-          paymentDateNumber: count.toString()
-        }
-      })
-      // 4. get all tochka payments from db
-      const [ account ] = await db.query.accounts({
-        where: {
-          number: process.env.TOCHKA_ACCOUNT_CODE_IP
-        }
-      }, '{ id }')
-      const [ payments, orgs ] = await Promise.all([
-        db.query.payments({
-          where: {
-            account: {
-              id: account.id
-            }
-          }
-        }, '{ id dateLocal tochkaId }'),
-        db.query.orgs({}, '{ id inn }')
-      ])
-      // 5. write new payments to db
-      //    and also write new counterparties into Org db table
-      const handled = await Promise.all(tochkaPaymentsAugmented.map(({
-        payment_bank_system_id: tochkaId,
-        payment_date,
-        payment_amount,
-        paymentDateNumber,
-        counterparty_inn: inn,
-        counterparty_name,
-        payment_purpose: purpose,
-      }) => {
-        const payment = payments.find(p => p.tochkaId === tochkaId)
-        if (payment) return
-        const org = orgs.find(o => o.inn === inn)
-        if (!org) orgs.push({ inn })
-        const dateLocal = (payment_date.split('.').reverse().join('-') + 'T00:00:00.000Z')
-          .slice(0,-paymentDateNumber.length - 1)
-          + paymentDateNumber + 'Z'
-        return db.mutation.createPayment({
-          data: {
-            tochkaId,
-            dateLocal,
-            amount: Math.abs(parseFloat(payment_amount)),
-            isIncome: parseFloat(payment_amount) > 0,
-            purpose,
-            account: {
-              connect: {
-                id: account.id
-              }
-            },
-            org: {
-              ...org 
-                ? {
-                  connect: {
-                    id: org.id
-                  }
-                }
-                : {
-                  create: {
-                    inn,
-                    name: counterparty_name
-                      .replace('ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ','ООО')
-                      .replace('Индивидуальный предприниматель','ИП')
-                  }
-                }
-            }
-          }
-        }, '{ id amount isIncome org { inn name } }')
-      }))
-      const upserted = handled.filter(c => !!c) //filter out nulls
-      // console.log('upserted > ', JSON.stringify(upserted, null, 2))
-      return { count: upserted.length }
-    } catch (err) {
-      console.log('err > ', err)
-      throw err
+    })
+    const { request_id } = await requestResponse.json()
+    // 2. fetch payments from tochka server
+    const statementUrl = baseUrl + '/result/' + request_id
+    const statementResponse = await fetch(statementUrl, {
+      method: 'GET',
+      headers
+    })
+    const statusText = statementResponse.statusText
+		if (statusText !== 'OK') throw new Error('Ошибка сервера Точка. Статус запроса fetchTochkaPayments: ' + statusText)
+    const { payments } = await statementResponse.json()
+    // console.log('tochkaPayments > ', JSON.stringify(payments,null,2))
+    console.log('tochkaPayments.length > ', payments.length)
+    return payments
+}
+
+const formatTochkaPayments = async (_, payments, ctx, info) => {
+  const inns = payments.map(({ counterparty_inn: inn }) => inn)
+    .reduce((res, p) => [...res, ...res.includes(p) ? [] : [p]], [])
+  const orgs = await upsertOrgsByInn(_, inns, ctx, info)
+  console.log('orgs > ', orgs)
+  // count number of payments for each day to generate unique dateLocal
+  paymentDateCounter = {}
+  return payments.map(p => {
+    paymentDateCounter[p.payment_date] = count = 
+      (paymentDateCounter[p.payment_date] || 0) + 1
+    return {
+      amount: Math.abs(parseFloat(p.payment_amount)),
+      dateLocal: (p.payment_date.split('.').reverse().join('-') + 'T00:00:00.000Z')
+        .slice(0, -count.toString().length - 1)
+        + count + 'Z',
+      isIncome: parseFloat(p.payment_amount) > 0,
+      orgId: orgs.find(o => p.counterparty_inn === o.inn).id,
+      purpose: p.payment_purpose,
+      tochkaId: p.payment_bank_system_id,
     }
+  })
+}
+
+const tochka = {
+  async getTochkaPayments(_, __, ctx, info) {
+    const fetchedPayments = await fetchTochkaPayments()
+    return await formatTochkaPayments(_, fetchedPayments, ctx, info)
+  },
+	async syncWithTochkaPayments(_, __, ctx, info) {
+    const { userId, db } = ctx
+    const mode = 'syncAll' // TODO add 'syncNew' mode
+    const accountId = (await db.query.accounts({
+      where: { number: process.env.TOCHKA_ACCOUNT_CODE_IP }
+    }, '{ id }'))[0].id
+    const tochkaPayments = (await tochka.getTochkaPayments(_, __, ctx, info))
+    const payments = await db.query.payments({
+      where: { account: { id: accountId } }
+    }, '{ id dateLocal tochkaId }')
+    const tochkaIds = payments.map(({ tochkaId }) => tochkaId)
+    const toCreate = tochkaPayments.filter(p => !tochkaIds.includes(p.tochkaId)).map( p => {
+      const orgId = p.orgId
+      delete p.orgId
+      return {
+        ...p,
+        org: {
+          connect: {
+            id: orgId
+          }
+        }
+      }
+    })
+    const { payments: accountPayments } = await db.mutation.updateAccount({
+      where: { id: accountId },
+      data: {
+        payments: {
+          ...toCreate.length && {create: toCreate }
+        }
+      }
+    }, '{ id }')
+    return { count: toCreate.length }
+    // // 5. write new payments to db
+    // //    and also create missing counterparties' Orgs
+    // const handled = await Promise.all(tochkaPaymentsAugmented.map(async ({
+    //   payment_bank_system_id: tochkaId,
+    //   payment_amount,
+    //   dateLocal,
+    //   counterparty_inn: inn,
+    //   counterparty_name,
+    //   payment_purpose: purpose,
+    // }) => {
+    //   const payment = payments.find(p => p.tochkaId === tochkaId)
+    //   if (payment) return
+    //   // const moeDeloOrg = moeDeloOrgs.find(o => o.inn === inn)
+    //   const org = { moedeloId, name, legalAddress } = moeDeloOrgs.find(o => o.inn === inn)
+    //     ? orgs.find(o => o.moedeloId === moedeloId)
+    //       || await db.mutation.createOrg({
+    //         data: {
+    //           moedeloId,
+    //           name,
+    //           legalAddress
+    //         }
+    //       })
+    //     : await createOrg(_, { inn }, ctx, '{ id moedeloId inn name }')
+    //   // if (!org) orgs.push({ inn })
+    //   // const org = await createOrg(_, { inn }, ctx, '{ id moedeloId inn name }')
+    //   console.log('org > ', org)
+      // return db.mutation.createPayment({
+      //   data: {
+      //     tochkaId,
+      //     dateLocal,
+      //     amount: Math.abs(parseFloat(payment_amount)),
+      //     isIncome: parseFloat(payment_amount) > 0,
+      //     purpose,
+      //     account: {
+      //       connect: {
+      //         id: account.id
+      //       }
+      //     },
+      //     org: {
+      //       ...org 
+      //         ? {
+      //           connect: {
+      //             id: org.id
+      //           }
+      //         }
+      //         : {
+      //           create: {
+      //             inn,
+      //             name: counterparty_name
+      //               .replace('ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ','ООО')
+      //               .replace('Индивидуальный предприниматель','ИП')
+      //           }
+      //         }
+      //     }
+      //   }
+      // }, '{ id amount isIncome org { inn name } }')
+    // }))
+    // const upserted = handled.filter(c => !!c) //filter out nulls
+    // // console.log('upserted > ', JSON.stringify(upserted, null, 2))
+    // return { count: upserted.length }
+    // return { count: 0 }
   },
   // async tochkaOrgs (_, __, ctx, info) {
   //   const url = 'https://enter.tochka.com/api/v1/organization/list'
